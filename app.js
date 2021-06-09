2f468a463df36d4a42152a06b3a43fef00a00981
Fayyaz Alam <fayazalam.98@gmail.com>
2021-06-09 14:57:54 -0700

    Q3 done replace row by empty string

--- app.js
+++ app.js
@@ -2,15 +2,17 @@
 
 //Q2 on html
 //Q3 not done yet
-
+// function remove(row) {
+//     row.innerHTML = "";
+// }
 //Q4 on html
 
 //Q5 
 
-var display = document.getElementById("counter");
-var number = 0
-function add(value){
-    var sum = number + value;
-    number = sum;
-    display.innerHTML = number;
-}
+// var display = document.getElementById("counter");
+// var number = 0
+// function add(value){
+//     var sum = number + value;
+//     number = sum;
+//     display.innerHTML = number;
+// }
