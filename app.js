a5e59ffbcb34ed5ac1312d252b81d16c1df86831
Fayyaz Alam <fayazalam.98@gmail.com>
2021-06-09 12:53:28 -0700

    Q 5 done with js and html

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 //q1 on html
 
-//Q2
+//Q2 on html
+//Q3 not done yet
+
+//Q4 on html
+
+//Q5 
+
+var display = document.getElementById("counter");
+var number = 0
+function add(value){
+    var sum = number + value;
+    number = sum;
+    display.innerHTML = number;
+}
